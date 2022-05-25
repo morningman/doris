32a81dcbac8ce4b7a52b27ea596042971d827b9c
chunping <cece_mail@163.com>
2022-05-25 21:49:30 +0800

    Create stop_cluster.sh

---
+++ auto-deploy/stop_cluster.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]; then
+        echo "$0 CLUSTER"
+        exit 1
+fi
+
+CLUSTER=$1
+CLUSTER_DIR=/mnt/ssd01/cold_on_s3/$CLUSTER
+
+echo " ============= stop service=============="
+
+parallel-ssh -h $CLUSTER/fe_hosts -i "cd $CLUSTER_DIR/fe && ./bin/stop_fe.sh"
+parallel-ssh -h $CLUSTER/be_hosts -i "cd $CLUSTER_DIR/be && ./bin/stop_be.sh"
