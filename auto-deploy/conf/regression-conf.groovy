1af85e4f2a5d1dd65c62ebdaedcc19091c4f61ac
chunping <cece_mail@163.com>
2022-05-25 21:53:25 +0800

    Create regression-conf.groovy

---
+++ auto-deploy/conf/regression-conf.groovy
@@ -0,0 +1,50 @@
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+/* ******* Do not commit this file unless you know what you are doing ******* */
+
+// **Note**: default db will be create if not exist
+defaultDb = "regression_test"
+
+jdbcUrl = "jdbc:mysql://127.0.0.1:9030/?"
+jdbcUser = "root"
+jdbcPassword = ""
+
+feHttpAddress = "127.0.0.1:8030"
+feHttpUser = "root"
+feHttpPassword = ""
+
+// set DORIS_HOME by system properties
+// e.g. java -DDORIS_HOME=./
+suitePath = "${DORIS_HOME}/regression-test/suites"
+dataPath = "${DORIS_HOME}/regression-test/data"
+
+// will test <group>/<suite>.groovy
+// empty group will test all group
+testGroups = ""
+// empty suite will test all suite
+testSuites = ""
+// empty directories will test all directories
+testDirectories = ""
+
+// this groups will not be executed
+excludeGroups = ""
+// this suites will not be executed
+excludeSuites = ""
+// this directories will not be executed
+excludeDirectories = ""
+
+customConf1 = "test_custom_conf_value"
+
+// for test csv with header
+enableHdfs=false // set to true if hdfs is ready
+hdfsFs = "hdfs://127.0.0.1:9000"
+hdfsUser = "doris-test"
+hdfsPasswd = ""
+brokerName = "broker_name"
+
+s3Endpoint = "cos.ap-beijing.myqcloud.com"
+s3BucketName = "*****"
+ak = "*********"
+sk = "***********"
