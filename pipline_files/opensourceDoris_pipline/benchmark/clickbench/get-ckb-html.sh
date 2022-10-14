cfa28dc7f80c0f5801927fb7ced7a36d1533ff86
stephen <hello_stephen@qq.com>
2022-10-14 08:39:31 +0800

    fix

--- pipline_files/opensourceDoris_pipline/benchmark/clickbench/get-ckb-html.sh
+++ pipline_files/opensourceDoris_pipline/benchmark/clickbench/get-ckb-html.sh
@@ -5,8 +5,9 @@ doris_result=$(bash get-result-json.sh)
 # note:
 #     on GNU bash, version 4.2.46(2)-release (x86_64-koji-linux-gnu): echo $(echo '[0.4]') -> 0
 #     on GNU bash, version 5.0.17(1)-release (x86_64-pc-linux-gnu): echo $(echo '[0.4]') -> [0.4]
-# I don't know why, but use xargs can get around it...
-doris_result_for_sed=$(echo "$doris_result" | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g' | xargs)
+# I don't know why, at current, get around it...
+doris_result_for_sed=$(echo "$doris_result" | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g')
+doris_result_for_sed=$(echo "$doris_result_for_sed" | tr '\n' ' ')
 
 rm 'index.html'
 wget --continue 'https://benchmark.clickhouse.com/'
