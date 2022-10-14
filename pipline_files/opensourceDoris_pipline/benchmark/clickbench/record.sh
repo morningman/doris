d7d8867b1b663ce640e4643dda58f8a69f514f42
stephen <hello_stephen@qq.com>
2022-10-14 14:52:22 +0800

    add timestamp to html file in order to find by day more easier

--- pipline_files/opensourceDoris_pipline/benchmark/clickbench/record.sh
+++ pipline_files/opensourceDoris_pipline/benchmark/clickbench/record.sh
@@ -4,6 +4,9 @@ set -ex
 teamcity_pullRequest_number=%teamcity.pullRequest.number%
 build_id=%teamcity.build.id%
 
+ts=$(date '+%Y%m%d%H%M%S')
+html_file_name="${ts}_clickbench_pr_${build_id}.html"
+
 echo '-------------------------------------------------------------'
 echo "####record some info"
 bash get-doris-runtime-conf.sh
@@ -12,30 +15,30 @@ bash get-table-schema.sh
 echo '-------------------------------------------------------------'
 bash get-mechine-info.sh
 echo '-------------------------------------------------------------'
-bash get-ckb-html.sh | tee "clickbench_pr_${build_id}.html"
+bash get-ckb-html.sh | tee "$html_file_name"
 echo '-------------------------------------------------------------'
 
-echo "####upload clickbench.html to cos"
+echo "####upload $html_file_name to cos"
 set +x
 # shellcheck source=/dev/null
 source ~/.bashrc
 set -x
 coscli cp \
-    "clickbench_pr_${build_id}.html" \
-    cos://doris-community-test-1308700295/tmp/"clickbench_pr_${build_id}.html"
+    "$html_file_name" \
+    cos://doris-community-test-1308700295/tmp/"$html_file_name"
 
 set +x
 echo "####create an issue comment on github"
 bash create-an-issue-comment-on-github.sh \
     "$teamcity_pullRequest_number" \
-    "https://doris-community-test-1308700295.cos.ap-hongkong.myqcloud.com/tmp/clickbench_pr_${build_id}.html"
+    "https://doris-community-test-1308700295.cos.ap-hongkong.myqcloud.com/tmp/$html_file_name"
 
 echo -e "
 =============================================================
 
 wget the html below then open it with browser
 
-wget https://doris-community-test-1308700295.cos.ap-hongkong.myqcloud.com/tmp/clickbench_pr_${build_id}.html
+wget https://doris-community-test-1308700295.cos.ap-hongkong.myqcloud.com/tmp/$html_file_name
 
 =============================================================
 "
