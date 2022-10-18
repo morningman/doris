d014fce79d8aa5dd2e2114d9e02fc5c6acb9c6bb
stephen lee <lee@mbp.local>
2022-10-18 20:27:57 +0800

    fix total time, support mac install coscli

--- pipline_files/opensourceDoris_pipline/benchmark/clickbench/install-coscli.sh
+++ pipline_files/opensourceDoris_pipline/benchmark/clickbench/install-coscli.sh
@@ -10,8 +10,20 @@ if coscli --version; then
   exit
 fi
 
-wget https://github.com/tencentyun/coscli/releases/download/v0.12.0-beta/coscli-linux
-mv coscli-linux coscli
+# os type: linux, mac
+os_type='linux'
+if [[ -n "$1" ]]; then os_type="$1"; fi
+
+if [[ "$os_type" == 'linux' ]]; then
+  wget https://github.com/tencentyun/coscli/releases/download/v0.12.0-beta/coscli-linux
+  mv coscli-linux coscli
+elif [[ "$os_type" == 'mac' ]]; then
+  wget https://github.com/tencentyun/coscli/releases/download/v0.12.0-beta/coscli-mac
+  mv coscli-mac coscli
+else
+  echo 'Only linux or mac.'
+  exit 1
+fi
 chmod 755 coscli
 ./coscli --version
 
@@ -29,10 +41,16 @@ cos:
     endpoint: cos.ap-hongkong.myqcloud.com
 " >~/.cos.yaml
 
-#TODO, not effect?
-echo "
-export PATH=$(pwd):\$PATH
-" >>~/.bashrc
+PATH="$(pwd):$PATH"
+export PATH
+echo "export PATH=$(pwd):\$PATH" >>~/.bashrc
+coscli --version
 
-# shellcheck source=/dev/null
-source ~/.bashrc
+# max default shell is zsh
+if [[ "$os_type" == 'mac' ]]; then
+  # mac load order: /etc/profile /etc/paths ~/.bash_profile ~/.bash_login ~/.profile ~/.bashrc
+  echo "
+if [ -f ~/.bashrc ]; then
+   source ~/.bashrc
+fi" >>~/.zshrc
+fi
