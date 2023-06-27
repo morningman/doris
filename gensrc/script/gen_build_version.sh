297f0a0b7e54eaff70b6718c0377662df063112f
morningman <morningman@163.com>
2023-06-28 00:45:49 +0800

    1.2.4.4-for-scyh

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -31,7 +31,7 @@ build_version_prefix="doris"
 build_version_major=1
 build_version_minor=2
 build_version_patch=4
-build_version_rc_version="3"
+build_version_rc_version="4-for-scyh"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}-${build_version_rc_version}"
 
