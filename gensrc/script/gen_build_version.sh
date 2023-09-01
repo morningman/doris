bebab58c2b2c6c05ea4ba21bd76adbabb3be4683
morningman <morningman@163.com>
2023-09-01 11:44:31 +0800

    selectdb-doris-2.0.2-doris-2.0.1-rc04

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -30,7 +30,7 @@ set -eo pipefail
 build_version_prefix="selectdb-doris"
 build_version_major=2
 build_version_minor=0
-build_version_patch=1
+build_version_patch=2
 build_version_rc_version="doris-2.0.1-rc04"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}-${build_version_rc_version}"
