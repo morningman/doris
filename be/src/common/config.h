3445844401393528b4153fcc781f61399ab9bc0a
morningman <morningman@163.com>
2023-09-13 16:19:19 +0800

    [tmp] add max_file_scanner BE config

--- be/src/common/config.h
+++ be/src/common/config.h
@@ -1110,6 +1110,8 @@ DECLARE_mBool(enable_merge_on_write_correctness_check);
 // The secure path with user files, used in the `local` table function.
 DECLARE_mString(user_files_secure_path);
 
+DECLARE_mInt32(max_file_scanner_num);
+
 #ifdef BE_TEST
 // test s3
 DECLARE_String(test_s3_resource);
--- be/src/vec/exec/scan/new_file_scan_node.cpp
+++ be/src/vec/exec/scan/new_file_scan_node.cpp
@@ -59,7 +59,7 @@ Status NewFileScanNode::prepare(RuntimeState* state) {
 }
 
 void NewFileScanNode::set_scan_ranges(const std::vector<TScanRangeParams>& scan_ranges) {
-    int max_scanners = config::doris_scanner_thread_pool_thread_num;
+    int max_scanners = config::max_file_scanner_num;
     if (scan_ranges.size() <= max_scanners) {
         _scan_ranges = scan_ranges;
     } else {
