b66b89552c20961577e9447868e623f348849445
morningman <morningman@163.com>
2023-09-13 22:45:21 +0800

    2.0.4-doris-2.0.1-rc04-yunyigou

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -30,8 +30,8 @@ set -eo pipefail
 build_version_prefix="selectdb-doris"
 build_version_major=2
 build_version_minor=0
-build_version_patch=3
-build_version_rc_version="doris-2.0.1-rc04-jdbc-log"
+build_version_patch=4
+build_version_rc_version="doris-2.0.1-rc04-stable"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}.${build_version_patch}-${build_version_rc_version}"
 
