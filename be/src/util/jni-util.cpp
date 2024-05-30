4cffeb9aaae1502ceeaffda08fb3298d93867cb5
Xinyi Zou <zouxinyi02@gmail.com>
2024-05-30 18:29:34 +0800

    [fix](libjdk) Revert support loading libjvm at runtime (#35557)
    Revert #13660
    
    `-l` and `dlopen` load Dynamic Link Library, will use the Allocator of
    main program, Doris use Jemalloc by default.
    
    but, `dlopen` load libjvm.so and use Jemalloc compiled with prefix,
    overwriting malloc/free will incompatible with libjvm.so. see #34578 for
    details.
    
    In addition, jemalloc not recommend `dlopen` to load Dynamic Link
    Library, `dlclose` will memory leak in that case.
    https://github.com/jemalloc/jemalloc/issues/2404
    https://github.com/jemalloc/jemalloc/issues/1321
    https://github.com/jemalloc/jemalloc/issues/1890
    
    `export LD_PRELOAD` to force `libjvm.so` use a separate `jemalloc.so`
    will not solve the problem, but may cause a new crash.
    
    ```
    received by PID 2368 (TID 2391 OR 0x7f445cafb700) from PID 0; stack trace: ***
     0# doris::signal::(anonymous namespace)::FailureSignalHandler(int, siginfo_t*, void*) at /root/selectdb-core/be/src/common/signal_handler.h:421
     1# PosixSignals::chained_handler(int, siginfo*, void*) [clone .part.0] in /opt/jdk/lib/server/libjvm.so
     2# JVM_handle_linux_signal in /opt/jdk/lib/server/libjvm.so
     3# 0x00007F448DB40400 in /lib64/libc.so.6
     4# je_arena_dalloc_promoted at /root/selectdb-core/thirdparty/src/jemalloc-5.3.0/doris_build/../src/arena.c:1277
     5# je_free_default at /root/selectdb-core/thirdparty/src/jemalloc-5.3.0/doris_build/../src/jemalloc.c:3014
     6# __pthread_create_2_1 in /lib64/libpthread.so.0
     7# os::create_thread(Thread*, os::ThreadType, unsigned long) in /opt/jdk/lib/server/libjvm.so
     8# CompilerThread::CompilerThread(CompileQueue*, CompilerCounters*) in /opt/jdk/lib/server/libjvm.so
     9# CompileBroker::make_thread(CompileBroker::ThreadType, _jobject*, CompileQueue*, AbstractCompiler*, JavaThread*) [clone .constprop.0] in /opt/jdk/lib/server/libjvm.so
    10# CompileBroker::possibly_add_compiler_threads(JavaThread*) in /opt/jdk/lib/server/libjvm.so
    11# CompileBroker::compiler_thread_loop() in /opt/jdk/lib/server/libjvm.so
    12# JavaThread::thread_main_inner() in /opt/jdk/lib/server/libjvm.so
    13# Thread::call_run() in /opt/jdk/lib/server/libjvm.so
    14# thread_native_entry(Thread*) in /opt/jdk/lib/server/libjvm.so
    15# start_thread in /lib64/libpthread.so.0
    16# clone in /lib64/libc.so.6
    ```
    
    <!--Describe your changes.-->

--- be/src/util/jni-util.cpp
+++ be/src/util/jni-util.cpp
@@ -35,7 +35,7 @@
 #include "gutil/strings/substitute.h"
 #include "util/doris_metrics.h"
 #include "util/jni_native_method.h"
-#include "util/libjvm_loader.h"
+// #include "util/libjvm_loader.h"
 
 using std::string;
 
@@ -415,7 +415,7 @@ Status JniUtil::get_jni_scanner_class(JNIEnv* env, const char* classname,
 }
 
 Status JniUtil::Init() {
-    RETURN_IF_ERROR(LibJVMLoader::instance().load());
+    // RETURN_IF_ERROR(LibJVMLoader::instance().load());
 
     // Get the JNIEnv* corresponding to current thread.
     JNIEnv* env = nullptr;
