7de0107f430deab631a37bc1af2c8ddbf13213cf
morningman <morningman@163.com>
2024-06-13 18:34:30 +0800

    [Fix](sql function)Fix split_part function behavior #36231

--- be/src/vec/functions/function_string.h
+++ be/src/vec/functions/function_string.h
@@ -1851,6 +1851,11 @@ public:
                                         reinterpret_cast<const char*>(str.data + pre_offset + 1),
                                         (size_t)offset - pre_offset - 1},
                                 i, res_chars, res_offsets);
+                    } else if (num == 0) {
+                        StringOP::push_value_string(
+                                std::string_view {reinterpret_cast<const char*>(str.data),
+                                                  str.size},
+                                i, res_chars, res_offsets);
                     } else {
                         StringOP::push_null_string(i, res_chars, res_offsets, null_map_data);
                     }
@@ -1880,6 +1885,11 @@ public:
                                 std::string_view {reinterpret_cast<const char*>(
                                                           str.data + pre_offset + delimiter.size),
                                                   (size_t)offset - pre_offset - delimiter.size},
+                                i, res_chars, res_offsets);
+                    } else if (num == 0) {
+                        StringOP::push_value_string(
+                                std::string_view {reinterpret_cast<const char*>(str.data),
+                                                  str.size},
                                 i, res_chars, res_offsets);
                     } else {
                         StringOP::push_null_string(i, res_chars, res_offsets, null_map_data);
@@ -1920,6 +1930,10 @@ public:
                                         (size_t)pre_offset - offset - delimiter.size)},
                                 i, res_chars, res_offsets);
                     }
+                } else if (num == 0) {
+                    StringOP::push_value_string(
+                            std::string_view {reinterpret_cast<const char*>(str.data), str.size}, i,
+                            res_chars, res_offsets);
                 } else {
                     StringOP::push_null_string(i, res_chars, res_offsets, null_map_data);
                 }
