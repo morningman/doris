45a06250498ea94c3914286ee4862a49948c936b
morningman <morningman@163.com>
2024-06-15 23:15:19 +0800

    [tmp] kill connection when meeting Write mysql packet failed error

--- fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ConnectContext.java
@@ -884,9 +884,9 @@ public class ConnectContext {
     }
 
     // kill operation with no protect by timeout.
-    private void killByTimeout(boolean killConnection) {
-        LOG.warn("kill query from {}, kill mysql connection: {} reason time out", getRemoteHostPortString(),
-                killConnection);
+    public void killSelf(boolean killConnection, String reason) {
+        LOG.warn("kill query from {}, kill mysql connection: {} reason: {}", getRemoteHostPortString(),
+                killConnection, reason);
 
         if (killConnection) {
             isKilled = true;
@@ -941,7 +941,7 @@ public class ConnectContext {
         }
 
         if (killFlag) {
-            killByTimeout(killConnection);
+            killSelf(killConnection, "timeout connection");
         }
     }
 
